88073093a54d29f07b60baeafe69bc5a9e66a9d7
Carina <carinausbeck@live.dk>
2021-02-07 19:24:03 +0100

    first

---
+++ node4/private/date.js
@@ -0,0 +1,16 @@
+
+exports.currentDate = function() {
+    date = new Date(); 
+    let day = date.getDate(); 
+    let month = date.getMonth() + 1; //since January is 0
+    let year = date.getFullYear();
+
+    if(day < 10){
+        day = '0' + day; //putting zero in front of day
+    }
+    if(month < 10){
+        month = '0' + month; //putting zero in front of month
+    }
+
+    return (day + '-' + month + '-' + year);
+}
